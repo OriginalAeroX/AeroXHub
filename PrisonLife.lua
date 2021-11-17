0bfc92dd87cd402a9420b16c85f77b6b6aea416e
OriginalAeroX <94316810+OriginalAeroX@users.noreply.github.com>
2021-11-17 20:58:32 +0200

    Update PrisonLife.lua

--- PrisonLife.lua
+++ PrisonLife.lua
@@ -1,4 +1,4 @@
-    -- Prison Life
+-- Prison Life
     local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
     local Window = Library.CreateLib("AeroX Hub | Prison Life", "Ocean")
 
