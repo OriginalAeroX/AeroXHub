e2399f54cc211a54c0a1702392a1da59d58993cf
OriginalAeroX <94316810+OriginalAeroX@users.noreply.github.com>
2021-11-17 20:53:31 +0200

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/OriginalAeroX/AeroXHub/main/loader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OriginalAeroX/AeroXHub/main/loader.lua", true))()
