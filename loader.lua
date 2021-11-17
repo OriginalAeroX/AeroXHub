183d6fb44b8513ccbac9407af7512d72a2bbff14
OriginalAeroX <94316810+OriginalAeroX@users.noreply.github.com>
2021-11-17 20:33:49 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,11 +1,11 @@
 -- prison life
-if game.Placeid == 155615604
+if game.PlaceId == 155615604
   loadstring(game:HttpGet("https://github.com/OriginalAeroX/AeroXHub/blob/main/PrisonLife.lua"))()
 
 -- arsenal
-if game.Placeid == 286090429
+if game.PlaceId == 286090429
   loadstring(game:HttpGet("https://github.com/OriginalAeroX/AeroXHub/blob/main/Arsenal.lua",true))()
 
 -- ninja legends
-if game.Placeid == 3956818381
+if game.PlaceId == 3956818381
   loadstring(game:HttpGet("https://github.com/OriginalAeroX/AeroXHub/blob/main/NinjaLegends.lua",true))()
