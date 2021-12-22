1f90f2bc099340f9806764988452ca3b18f1a0e2
OriginalAeroX <94316810+OriginalAeroX@users.noreply.github.com>
2021-12-22 17:35:08 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,16 @@
-
+if game.PlaceId == 155615604 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/OriginalAeroX/game.scripts.net/main/PrisonLife.lua", true))()
+  return
+end
+if game.PlaceId == 3956818381 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/OriginalAeroX/game.scripts.net/main/NinjaLegends.lua", true))()
+  return
+end
+if game.PlaceId == 142823291 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/OriginalAeroX/game.scripts.net/main/MM2.lua", true))()
+  return
+end
+if game.PlaceId == 286090429 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/OriginalAeroX/game.scripts.net/main/Arsenal.lua", true))()
+  return
+end
